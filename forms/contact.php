f96629f70a900a61afcdd4196b60e69ae6c7706f
Khanh Trinh <kqtrinh87@gmail.com>
2022-11-20 23:21:00 -0600

    Added projects

--- forms/contact.php
+++ forms/contact.php
@@ -37,5 +37,7 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
+  $contact->honeypot = $_POST['first_name'];
   echo $contact->send();
+  
 ?>
